a80f88c3ea3637ca0adbc015ad55a97c66b9e6bb
unknown <kamilbienek@op.pl>
2023-01-15 19:24:38 +0100

    xml schema validation added

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import sys
 from antlr4 import *
+import xmlschema
 
-from GrammarErrorListener import GrammarErrorListener
 from XMLLexer import XMLLexer
 from XMLParser import XMLParser
 from XMLParserListener import XMLParserListener
 
 
-class Validator:
+class Validator():
     def __init__(self):
         self.files = "./files/"
+        self.schemas = "./schemas/"
         self.input = None
+        self.schema = None
         self.code = ""
+        self.with_schema = False
 
-    def loadFile(self):
-        if len(sys.argv) > 1:
+    def loadFiles(self):
+        if len(sys.argv) == 2:
             self.input = sys.argv[1]
+            print("No schema attached.")
+            return True
+        elif len(sys.argv) == 3:
+            self.input = sys.argv[1]
+            self.schema = xmlschema.XMLSchema(self.schemas + sys.argv[2])
+            self.with_schema = True
             return True
         else:
-            print("missing input file")
+            print("Missing input file")
             return False
 
     def readFile(self):
@@ -27,19 +36,23 @@ class Validator:
 
     def validate(self):
         lexer = XMLLexer(InputStream(self.code))
-        grammarErrorListener = GrammarErrorListener()
-        lexer.addErrorListener(grammarErrorListener)
         stream = CommonTokenStream(lexer)
         parser = XMLParser(stream)
-        parser.addErrorListener(grammarErrorListener)
         tree = parser.document()
         walker = ParseTreeWalker()
         listener = XMLParserListener()
         walker.walk(listener, tree)
 
+        if self.with_schema:
+            if self.schema.is_valid(self.files + self.input):
+                print("XML file seems to have correct syntax and is compatible with given schema.")
+            else:
+                print("XML file not compatible with given schema.")
+        else:
+            print("XML file seems to have correct syntax. Please provide XML schema file (XSD) in order to fully check the file.")
 
 if __name__ == '__main__':
     validator = Validator()
-    if validator.loadFile():
+    if validator.loadFiles():
         validator.readFile()
         validator.validate()
